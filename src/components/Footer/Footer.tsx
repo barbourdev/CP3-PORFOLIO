045416c266bfb512d31b52b86366f8087da88b80
kaiquepintor <kaiquepintor.kp@gmail.com>
2024-11-05 20:49:18 -0300

    Adição e criação do footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function Footer() {
+
+    return (
+        <footer className="mx-auto w-full max-w-container px-4 sm:px-6 lg:px-8 bg-white" aria-labelledby="footer-heading">
+            <div className="items-centers justify-between gap-4 border-t border-gray-100 py-6 md:grid-cols-2">
+                <p className="text-sm/6 text-black text-center">
+                    © 2024 - 1TDSPO FIAP
+                </p>
+            </div>
+        </footer>
+    );
+}
