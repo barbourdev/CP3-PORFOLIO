be1b9093603cb588bd93b8f8bbc24af48ae87403
barbourdev <felipe.barbourf@gmail.com>
2024-11-05 21:02:28 -0300

    Correções de responsivdade

--- src/app/portfolios/page.tsx
+++ src/app/portfolios/page.tsx
@@ -31,7 +31,7 @@ export default function Portfolios() {
   }, []);
 
   return (
-    <div className="h-screen p-8">
+    <div className="p-8">
         <h1 className="text-3xl font-bold text-center mb-8">Portfólios</h1>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
